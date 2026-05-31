da33cbc4792783d8561633005f97d4b9050468f8
Adib Ashraf <aa.adibashraf@gmail.com>
2026-05-31 23:19:04 +0600

    feat: bold+italic session text, bold active window text

--- status/session.sh
+++ status/session.sh
@@ -2,7 +2,7 @@ show_session() {
   local index=$1
   local icon=$(get_tmux_option "@catppuccin_session_icon" "")
   local color=$(get_tmux_option "@catppuccin_session_color" "#{?client_prefix,$thm_red,$thm_green}")
-  local text=$(get_tmux_option "@catppuccin_session_text" "#S")
+  local text=$(get_tmux_option "@catppuccin_session_text" "#[bold,italics]#S#[nobold,noitalics]")
 
   local module=$( build_status_module "$index" "$icon" "$color" "$text" )
 
