5ede7eda9050915e299cdfebb5bea8beb7afb36e
Adib Ashraf <aa.adibashraf@gmail.com>
2026-05-31 23:23:44 +0600

    fix: wrap module output with bold/italic instead of embedding in text

--- status/session.sh
+++ status/session.sh
@@ -2,9 +2,9 @@ show_session() {
   local index=$1
   local icon=$(get_tmux_option "@catppuccin_session_icon" "")
   local color=$(get_tmux_option "@catppuccin_session_color" "#{?client_prefix,$thm_red,$thm_green}")
-  local text=$(get_tmux_option "@catppuccin_session_text" "#[bold,italics]#S#[nobold,noitalics]")
+  local text=$(get_tmux_option "@catppuccin_session_text" "#S")
 
   local module=$( build_status_module "$index" "$icon" "$color" "$text" )
 
-  echo "$module"
+  echo "#[bold,italics]${module}#[nobold,noitalics]"
 }
